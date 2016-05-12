46aebd9fc5206e8bf46c53b871e210ae6af9ac67
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-05-12 16:48:51 -0400

    allows node version passed as template

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xe
+set -e
 
 NODE_VERSION=0.12
 
@@ -13,8 +13,13 @@ IMAGE_WITH_DEPS_NAME=$NAME-npm-deps:$PACKAGE_SHA
 
 EXISTING_IMAGE=$(docker images -q $IMAGE_WITH_DEPS_NAME)
 if [ "$EXISTING_IMAGE" == "" ]; then
-  docker build -t $IMAGE_WITH_DEPS_NAME -f DockerDeps .
+  echo "Base NPM dependencies layer not found, building..."
+  echo "FROM mhart/alpine-node:$NODE_VERSION">DockerDepsTemp
+  cat DockerDeps>>DockerDepsTemp
+  docker build -t $IMAGE_WITH_DEPS_NAME -f DockerDepsTemp .
   echo "Built NPM dependencies layer"
+else
+  echo "Base NPM dependencies already in existing docker image"
 fi
 
 echo "FROM $IMAGE_WITH_DEPS_NAME">DockerTestFile
@@ -23,8 +28,9 @@ cat DockerNpmTest>>DockerTestFile
 docker build -t $NAME:$NODE_VERSION -f DockerTestFile .
 echo "Built docker image with source code"
 
-echo "Running final docket image"
 CONTAINER_NAME=$NAME-$NODE_VERSION
+echo "Stopping and removing any existing containers"
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
+echo "Running final docker image $CONTAINER_NAME"
 docker run --name $CONTAINER_NAME $NAME:$NODE_VERSION
