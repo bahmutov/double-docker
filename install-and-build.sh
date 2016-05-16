c7fd92aa245e5a0bd7cd87bf9eb9cc4203939e82
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-05-16 14:54:12 -0400

    fix(link): fixed finding the script folder from links

--- install-and-build.sh
+++ install-and-build.sh
@@ -10,7 +10,16 @@ if [ "$NODE_VERSION" == "" ]; then
 fi
 
 # the folder with this script (for calling our utility scripts)
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative
+  # to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+# echo "Script folder $DIR"
 
 . $DIR/utils/compute-sha.sh
 . $DIR/utils/build-base-image.sh
